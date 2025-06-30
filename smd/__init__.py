722911ba51ce80aeaf1c24d26e9b4a1188efdcb6
Zenitsu <174907517+nayandas69@users.noreply.github.com>
2025-06-30 20:45:18 +0600

    __init__.py set

--- smd/__init__.py
+++ smd/__init__.py
@@ -1,3 +1,22 @@
 # Social Media Downloader
-# __init__.py
+# smd/__init__.py
 # This file is part of Social Media Downloader (SMD).
+
+
+from .downloader import (
+    download_youtube_or_tiktok_video,
+    download_instagram_post,
+    extract_instagram_video_mp3,
+    batch_download_from_file,
+    check_for_updates,
+    show_help,
+)
+
+__all__ = [
+    "download_youtube_or_tiktok_video",
+    "download_instagram_post",
+    "extract_instagram_video_mp3",
+    "batch_download_from_file",
+    "check_for_updates",
+    "show_help",
+]
